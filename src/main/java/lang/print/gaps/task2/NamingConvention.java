530635f293f4308801c7934ec673ed023003cd3d
asliddin4949 <asliddin4949@inbox.ru>
2022-11-30 10:38:15 +0500

    changes added

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -5,9 +5,9 @@ public class NamingConvention {
         int variable = 10;
         int variableOne = 1;
         int variableTwo = 2;
-        int variable_three = 3;
-        final int variableFour = 4;
-        final int variableFive = 5;
+        int variableThree = 3;
+        final int VARIABLE_FOUR = 4;
+        final int VARIABLE_FIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
         int VariableEight = 8;
